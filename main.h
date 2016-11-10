c8984bc334d4b357b4979d96188d915bcfead34e
SANADA So <1rinkaku.2kiriandmayu@gmail.com>
2016-11-10 20:56:10 +0900

    家で作成した横スクゲーム本編

---
+++ main.h
@@ -0,0 +1,53 @@
+﻿#ifndef __MAIN_H__
+#define __MAIN_H__
+#pragma once
+
+struct UI
+{
+	int x;
+	int y;
+};
+
+struct Map {
+	const int max_x;
+	const int max_y;
+};
+
+struct Tornev
+{
+	int x; //トルネフのx,y座標
+	int y;
+	int y_prev;
+	int y_temp;
+	int jump_y;
+	int speed;
+	bool direction;
+	bool jump_flag_1;
+	bool jump_flag_2;
+};
+
+struct Item
+{
+	int x;
+	int y;
+	int speed;
+	bool direction;
+	bool flag;
+};
+
+struct Enemy
+{
+	int x;
+	int y;
+	int speed;
+	bool direction;
+	bool flag;
+	bool dead;
+};
+
+struct BGA
+{
+	int x;
+	int y;
+};
+#endif
